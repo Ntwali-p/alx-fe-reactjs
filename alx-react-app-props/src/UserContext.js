3ddadae2b3c53ee12fd962146dc5472bb3bdebb0
Ntwali-p <ntwali.pacific@gmail.com>
2025-09-02 20:07:35 +0200

    changes

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,5 +1,7 @@
 import { createContext } from 'react';
 
+// Create a new context
 const UserContext = createContext(null);
 
+// Export it so other components can use it
 export default UserContext;
