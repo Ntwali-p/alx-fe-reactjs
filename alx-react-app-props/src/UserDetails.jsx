e6992da06c730c733af435cfca1e0ac2b3baf2a0
Ntwali-p <ntwali.pacific@gmail.com>
2025-09-02 20:29:01 +0200

    changes

--- alx-react-app-props/src/UserDetails.jsx
+++ alx-react-app-props/src/UserDetails.jsx
@@ -1,12 +1,12 @@
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 import UserContext from './UserContext';
 
 function UserDetails() {
   const userData = useContext(UserContext);
 
   return (
-    <div style={{ padding: '15px', border: '1px solid gray', borderRadius: '8px', width: '300px', margin: '20px auto', textAlign: 'center' }}>
-      <p style={{ fontWeight: 'bold' }}>Name: {userData.name}</p>
+    <div>
+      <p>Name: {userData.name}</p>
       <p>Email: {userData.email}</p>
     </div>
   );
