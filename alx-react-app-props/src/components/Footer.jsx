2bf80b709955744e386dc6ebbb4d83b8341677a8
Ntwali-p <ntwali.pacific@gmail.com>
2025-09-02 19:27:19 +0200

    changes

---
+++ alx-react-app-props/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: '#222', color: '#fff', textAlign: 'center', padding: '10px', marginTop: '20px' }}>
+      <p style={{ margin: 0 }}>© 2025 My Favorite Cities. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
